08e620ddfdef49780ced6d20cfcb1db6d4b50f47
Raf S <ryfat5mart2007@gmail.com>
2026-04-10 16:23:04 +0500

    fix env for vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,9 +2,8 @@
 /// <reference types="node" />
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL || 'https://jkcsgpzzioyochxcqupc.supabase.co'
-const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprY3NncHp6aW95b2NoeGNxdXBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTc2NDMsImV4cCI6MjA5MTM5MzY0M30.rdnFwnyykkyDpS7VzE6bAK9LWBZnfrmSt9YQ260DwUw'
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 export type Profile = {
